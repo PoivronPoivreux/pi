23a5b53db9d7a0aadaf5c2d7ed663c54436e5e0f
PoivronPoivreux <100969394+PoivronPoivreux@users.noreply.github.com>
2023-06-28 09:31:13 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ownCloud</title>
+</head>
+<body>
+    <script>
+        window.location.href = 'owncloud/';
+    </script>
+</body>
+</html>
